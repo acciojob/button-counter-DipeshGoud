7777d84566d8cd1aff0efc2805923e1527a601b4
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-07-12 11:20:46 +0000

    submit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,16 @@
 import react from "react";
 
 let count = 0;
-{
-    let clickme = document.getElementById("clickMe");
-    clickme.addEventListner('click',increaseCount);
-
     let increaseCount = () =>{
         count++;
     }
-}
 
 const Counter = () => {
     return (
         <div>
         <p>Button Clicked {count} times</p>
         <br></br>
-        <button id="clickMe">Click me</button>
+        <button id="clickMe" onClick={increaseCount}>Click me</button>
         </div>
     )
 }
